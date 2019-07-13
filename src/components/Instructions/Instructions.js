3d6c5d9e039bcc80954eaaf7da4abc7e4ca243a3
IDM.air <innanadm8@gmail.com>
2019-07-13 17:04:03 -0400

    creating...

---
+++ src/components/Instructions/Instructions.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./instructions.css";
+
+const Instructions = () => {
+  return (
+    <div>
+      <p className="flow-text">
+        Focus is a game of memory. 24 images will randomly render. Click every
+        image once.
+      </p>
+    </div>
+  );
+};
+
+export default Instructions;
